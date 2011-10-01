cb5a582bc2f506f06ea67f08761abc686dcd61eb
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2011-10-01 14:00:49 +0200

    More playing with the API: - added a base layer selector, search box, layers box - render the Swisstrains tracks from a KML file

--- static/js/map.js
+++ static/js/map.js
@@ -1,9 +1,47 @@
 $(document).ready(function(){
-    var geo = new GeoAdmin.API();
-    geo.createMap({
+    var api = new GeoAdmin.API({
+        lang: 'en'
+    });
+    api.createMap({
         div: 'map',
-        easting: 720000,
-        northing: 90000,
+        easting: 680000,
+        northing: 210000,
         zoom: 5
     });
+    api.map.addLayerByName('ch.swisstopo.vec25-wander');
+    api.createSwissSearchCombo({
+       width: 500,
+       renderTo: 'box_search',
+       ref: 'geoadmin'
+    });
+    api.createBaseLayerTool({
+        renderTo: 'box_baselayer',
+        label: 'Orthophoto',
+        slider: {
+            width: 170
+        },
+        combo: {
+            width: 242
+        }
+    });
+    api.createLayerTree({
+        renderTo: 'box_layers',
+        width: 285
+    });
+    
+    var layer_kml = new OpenLayers.Layer.Vector('Swisstrains tracks', {
+        strategies: [new OpenLayers.Strategy.Fixed()],
+        protocol: new OpenLayers.Protocol.HTTP({
+            url: 'feed/fetch_kml.php',
+            format: new OpenLayers.Format.KML(),
+            
+        }),
+        style: {
+            strokeColor: "#ffff00",
+            strokeWidth: 4,
+            strokeOpacity: 0.75
+        },
+        projection: new OpenLayers.Projection('EPSG:4326')
+    });
+    api.map.addLayer(layer_kml);
 });
