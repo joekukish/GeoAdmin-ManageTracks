3dc729dd45b5721005228071761abca72a9b9d18
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2011-10-03 00:13:41 +0200

    Added FT library from http://code.google.com/p/fusion-tables-client-php

---
+++ api/fusion-tables-client-php/constants.php
@@ -0,0 +1,9 @@
+<?php
+
+define('SCOPE', 'https://www.google.com/fusiontables/api/query');
+define('SERVER_URI', 'https://www.google.com');
+define('GOOGLE_OAUTH_REQUEST_TOKEN_API', 'https://www.google.com/accounts/OAuthGetRequestToken');
+define('GOOGLE_OAUTH_ACCESS_TOKEN_API', 'https://www.google.com/accounts/OAuthGetAccessToken');
+define('GOOGLE_OAUTH_AUTHORIZE_API', 'https://www.google.com/accounts/OAuthAuthorizeToken');
+
+?>
